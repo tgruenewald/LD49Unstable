3c5c60069262ea9f27b23103d49a45d6ff61a5a9
tgruenewald <terrygruenewald@gmail.com>
2021-10-02 13:18:41 -0600

    click freeze no longer needs platform obj set

--- LD49Unstable/Assets/Prefab/ClickFreeze.cs
+++ LD49Unstable/Assets/Prefab/ClickFreeze.cs
@@ -13,6 +13,7 @@ public class ClickFreeze : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        platform = gameObject;
         platform.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
