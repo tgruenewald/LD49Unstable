1320c5463252dd4aa1804322e2653968aac098f7
tgruenewald <terrygruenewald@gmail.com>
2021-10-02 13:00:27 -0600

    player to jump to platforms and have the camera follow him

--- LD49Unstable/Assets/Scripts/PlaceholderChar.cs
+++ LD49Unstable/Assets/Scripts/PlaceholderChar.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlaceholderChar : MonoBehaviour
 {
     public float maxSpeed = 2f;
+    public LayerMask whatIsGround;
+    private CircleCollider2D groundCheck;
+    public bool grounded = true;
+    public float JumpForce = 400f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        groundCheck = GetComponent<CircleCollider2D>();
+        Camera.main.GetComponent<SmoothCamera>().target = gameObject;
     }
 
     // Update is called once per frame
@@ -16,5 +21,14 @@ public class PlaceholderChar : MonoBehaviour
     {
         float move = Input.GetAxis("Horizontal");
         GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        grounded = groundCheck.IsTouchingLayers (whatIsGround);
+
+        if (grounded && Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("jumping");
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpForce));
+        }
+
     }
 }
