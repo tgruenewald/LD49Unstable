4363b8618d9001cd61208d1e7b4a8fea9705d3de
tgruenewald <terrygruenewald@gmail.com>
2021-10-02 11:45:45 -0600

    door works with key

---
+++ LD49Unstable/Assets/Door.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        if (GameState.hasKey)
+        {
+           GameState.hasKey = false;
+            // Load next scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+
+    }
+}
